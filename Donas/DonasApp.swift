562f67d59cd33df639346e2e6167a3f250cf241d
parisollie <pjff18@gmail.com>
2025-03-21 10:23:35 -0600

    Initial Commit

---
+++ Donas/DonasApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonasApp.swift
+//  Donas
+//
+//  Created by Paul F on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DonasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Donas/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Donas
+//
+//  Created by Paul F on 21/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
